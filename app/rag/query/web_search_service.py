41a1b62da210b99567e58e479cec68b594cbc734
ZaneX <1393895459@qq.com>
2026-07-13 16:11:20 +0800

    feat(query): implement and test node_web_search_mcp

--- app/rag/query/web_search_service.py
+++ app/rag/query/web_search_service.py
@@ -1,5 +1,55 @@
-from app.process.query.agent.state import QueryGraphState
+import asyncio
+import json
 
+from agents.mcp import MCPServerStreamableHttp
+
+from app.process.query.agent.state import QueryGraphState
+from app.shared.runtime.logger import logger
+from app.infra.config.providers import infra_config
+
+
+def validate_and_get_query(state: QueryGraphState) -> str:
+
+    rewritten_query = state.get("rewritten_query", "")
+
+    if not rewritten_query:
+        logger.error("重写的问题为空，业务无法继续！")
+        raise ValueError("重写的问题为空，业务无法继续！")
+
+    return rewritten_query
+
+async def open_ai_mcp(rewritten_query: str):
+    # 连接mcp服务端
+    mcp_server = MCPServerStreamableHttp(
+        name="Streamable HTTP Python Server",
+        params={
+            "url": infra_config.mcp_config.mcp_base_url,
+            "headers": {"Authorization": f"Bearer {infra_config.mcp_config.api_key}"},
+            "timeout": 50,
+        },
+        cache_tools_list=True,
+        max_retry_attempts=3,
+    )
+
+
+    try:
+        # mcp服务连接
+        await mcp_server.connect()
+        # mcp工具调用
+        mcp_result = await mcp_server.call_tool(
+            tool_name="bailian_web_search",
+            arguments={
+                "query": rewritten_query,
+                "count": 5
+            }
+        )
+
+        return mcp_result
+    except Exception as e:
+        logger.error(f"mcp调用出错，问题: {str(e)}")
+    finally:
+        # 清空连接
+        await mcp_server.cleanup()
 
 def search_by_web(state: QueryGraphState) -> QueryGraphState:
     """
@@ -9,4 +59,15 @@ def search_by_web(state: QueryGraphState) -> QueryGraphState:
     3. 包含标题、链接和摘要
     4. 回写 web_search_docs
     """
-    return state
+
+    rewritten_query = validate_and_get_query(state)
+
+    # async使用openai提供的mcp调用方式
+    mcp_result = asyncio.run(open_ai_mcp(rewritten_query))
+
+    # 结果解析
+    text = mcp_result.content[0].text
+    text_dict = json.loads(text)
+    web_search_docs = text_dict.get("pages", [])
+
+    return web_search_docs
